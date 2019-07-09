44dd8e0568356f73e3cdbb2c3477ca31c0fd946e
Nikitas-io <nikitas.io@outlook.com>
2019-07-09 16:39:30 +0300

    Added sfx and more animations

--- js/logs.js
+++ js/logs.js
@@ -1,7 +1,26 @@
+const button = new Audio('sfx/button.wav');
+button.volume = 0.3;
+const typingLetter = new Audio('sfx/typingletter.wav');
+typingLetter.volume = 0.1;
+
+//Play the sound.
+var soundFlag = true;
+// SFX player.
+function soundRestart(audio) {
+    if (soundFlag) {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.currentTime = 0;
+        }
+    }
+}
+
+
 //Show logs.
 var showText = function (target, message, index, interval) {
-    console.log('Happens');
     if (index < message.length) {
+        soundRestart(typingLetter);
         document.getElementById(target).innerHTML += message[index++];
        // soundRestart("typing");
         setTimeout(function () {
@@ -11,5 +30,22 @@ var showText = function (target, message, index, interval) {
 }
 
 
+// Log Entries.
+const logEntries = [];
+logEntries[1] = "When it all started I had no idea how far deep it goes... At this point, I'm too \
+invested to ever look back."
 
-showText("log-entry-content", "HERE IS THE LOG ENTRY.", 0, 50);
+// Start the log.
+const logButton = document.getElementById('log-button');
+logButton.addEventListener('click', function(){
+    soundRestart(button);
+    // Print out the log entry.
+    showText("log-entry-content", logEntries[1], 0, 50);
+    // Make the log-entry button dissapear.
+    this.style.opacity = 0;
+    this.classList.remove('blinker');
+    // Remove the button from the DOM, after it fades away.
+    setTimeout(function () {
+        logButton.style.display = 'none';
+    }, 300);
+});
