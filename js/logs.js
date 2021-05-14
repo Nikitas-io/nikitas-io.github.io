188bfbc81a2a9c37a7d3412ec103f07ada004b75
Nikitas IO <chrononautic@gmail.com>
2021-05-14 08:22:03 +0300

    Animating the name

--- js/logs.js
+++ js/logs.js
@@ -122,3 +122,28 @@ imageContainer.addEventListener('click', function(evt){
         }
     }
 });
+
+
+
+/* Log out Nikitas */
+
+const retype = document.querySelector("#retype");
+const word = document.querySelector("h1 span");
+
+// reset the transition by...
+retype.addEventListener(
+	"click",
+	function (e) {
+		e.preventDefault;
+
+		// -> removing the class
+		word.classList.remove("animating");
+
+		// -> triggering reflow /* The actual magic */
+		void word.offsetWidth;
+
+		// -> and re-adding the class
+		word.classList.add("animating");
+	},
+	false
+);
