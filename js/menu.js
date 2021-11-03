441221d0b06ec4c8596cd5292d2cf4903c5953c9
Mona Lisa <chrononautic@gmail.com>
2021-11-03 03:56:25 +0200

    Made the page navigation menu responsive

---
+++ js/menu.js
@@ -0,0 +1,83 @@
+const menu = document.getElementById("menubar");
+const burger = document.getElementById('hamburger');
+const closeBurger = document.getElementById("close-burger");
+
+burger.addEventListener('click', function () {
+  toggleMenu();
+});
+
+closeBurger.addEventListener('click', function () {
+  toggleMenu();
+});
+
+let outsideBurger = '#menubar *';
+document.body.addEventListener('click', function(event){
+  // Check if the side-menu is opened.
+  if (menu.classList.contains('open')){
+    // Only close the menu if the user has clicked outside the menu.
+    if (!event.target.matches(outsideBurger) && event.target != burger){
+      toggleMenu();
+    }
+  }
+});
+
+function toggleMenu() {
+  //If open close it, if closed open it.
+  if (!menu.classList.contains('open')) {
+    menu.classList.add('open');
+    burger.style.opacity = 0;
+  }else{
+    menu.classList.remove('open');
+    burger.style.opacity = 1;
+  }
+}
+
+/*--------TOUCH EVENTS---------*/
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches // browser API
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    // Most significant
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+      /* left swipe */
+      if ( xDiff > 0 ) {
+        // Check if the side-menu is closed so you can open it.
+        if (menu.classList.contains('open')){
+            // Close the menu.
+            toggleMenu();
+        }
+      }
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
