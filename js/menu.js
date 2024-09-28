80ef4c48708eaf4553436ced07de96fa27e4d5ef
Nikitas I/O <chrononautic@gmail.com>
2024-09-29 00:22:19 +0300

    Attempt 3

--- js/menu.js
+++ js/menu.js
@@ -10,6 +10,18 @@ const word = document.querySelector("h1 span");
 const loadingDots = document.getElementsByClassName('loading-dots');
 const scrollIndicator = document.getElementsByClassName('arrows');
 
+// First we get the viewport height and we multiply it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // Update the value of vh on resize
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 window.addEventListener('DOMContentLoaded', (evt) => {
   "use strict";
 
